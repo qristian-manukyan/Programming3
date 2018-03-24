df7ab8286aec2e074c94053a554d4db09dbea0f5
Qristian Manukyan <qristian.m.y@tumo.org>
2018-03-24 11:25:04 +0400

    Created new class Organism that handles all the properties and methods the living creatures have and extended Grass class referencing Organism class using javascript object inheritance.

---
+++ finalProject/universe.js
@@ -0,0 +1,92 @@
+var matrix = [];
+var side = 20;
+var w = 28;
+var h = w;
+var grassArr = [];
+var xotakerArr = [];
+var gishatichArr = [];
+var amenakerArr = [];
+var weather = 0;
+
+
+function fillMatrix(width, height) {
+  var m = [];
+  for (var y = 0; y < width; y++) {
+    m.push([]);
+    for (var x = 0; x < height; x++) {
+      m[y].push(0);
+    }
+    m[0][0] = 1;
+  }
+  return m;
+}
+
+function createObjects(mtx=matrix) {
+  grassArr = [];
+  xotakerArr = [];
+  gishatichArr = [];
+  for (var y = 0; y < mtx.length; y++) {
+    for (var x = 0; x < mtx[y].length; x++) {
+      if (mtx[y][x] == 1) {
+        grassArr.push(new Grass(x, y));
+      }
+      else if (mtx[y][x] == 2) {
+        xotakerArr.push(new Xotaker(x, y));
+      }
+      else if (mtx[y][x] == 3) {
+        gishatichArr.push(new Gishatich(x, y));
+      }
+      else if (mtx[y][x] == 4) {
+        amenakerArr.push(new Amenaker(x, y));
+      }
+    }
+  }
+}
+
+function drawGrid(mtx=matrix) {
+  for (var y = 0; y < mtx.length; y++) {
+    for (var x = 0; x < mtx[y].length; x++) {
+      //noStroke();
+      if (mtx[y][x] == 0) {
+        fill('#acacac');
+        rect(x * side, y * side, side, side);
+      }
+      else if (mtx[y][x] == 1) {
+        fill('green');
+        rect(x * side, y * side, side, side);
+      }
+      else if (mtx[y][x] == 2) {
+        fill('yellow');
+        rect(x * side, y * side, side, side);
+      }
+      else if (mtx[y][x] == 3) {
+        fill('red');
+        rect(x * side, y * side, side, side);
+      }
+      else if (mtx[y][x] == 4) {
+        fill('brown');
+        rect(x * side, y * side, side, side);
+      }
+      else {
+        fill('blue');
+        rect(x * side, y * side, side, side);
+      }
+    }
+  }
+}
+
+function populate() {
+  for (var i in grassArr) {
+    grassArr[i].evolve();
+  }
+  for (var i in xotakerArr) {
+    xotakerArr[i].hunt();
+  }
+  for (var i in gishatichArr) {
+    gishatichArr[i].hunt();
+  }
+  for (var i in amenakerArr) {
+    amenakerArr[i].hunt();
+  }
+  drawGrid();
+}
